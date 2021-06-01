68030a92c95e02ae238ab36b877571b206ea0d32
unknown <yadav150mohit@gmail.com>
2021-06-02 00:43:41 +0530

    using encryption and environment variable

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt= require("mongoose-encryption");
 const ejs = require("ejs");
 
 const app = express();
@@ -13,10 +15,15 @@ app.use(express.static("public"));
 
  mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology : true});
 
-  const userSchema = {
+
+
+  const userSchema = new mongoose.Schema({
      username : String,
      password : String
-  };
+  });
+
+
+  userSchema.plugin(encrypt, {secret: process.env.SECRET,encryptedFields: ['password'] });
 
   const User= mongoose.model("User",userSchema);
 
